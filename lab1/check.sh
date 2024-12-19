6f526c7e3eb78d97a3224796e97fb850b819b403
Tarasov Artyom <ATarasov9@alfabank.ru>
2024-12-19 23:33:02 +0300

    lab2: draft

--- lab1/check.sh
+++ lab1/check.sh
@@ -1,4 +1,4 @@
-for n in 10000 100000 1000000 10000000 100000000
+for n in 10000000 100000000 1000000000 10000000000
 do
-    docker run -v $(pwd)/result:/Downolads cpu-load-app ./load_cpu $n >> container_times.txt
+   docker run -it --rm cpu-load-app /usr/bin/time ./load_cpu $n 2>> container_cpu_times.txt
 done
